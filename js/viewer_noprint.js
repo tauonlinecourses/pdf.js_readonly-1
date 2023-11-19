ba821069a86f61c025337ebc01b55a3d6f6a8be9
Tau online <tauonlinecourses@gmail.com>
2023-11-19 11:51:01 +0200

    update 3

--- js/viewer_noprint.js
+++ js/viewer_noprint.js
@@ -79,7 +79,7 @@ const defaultOptions = {
      *  You also can protect PDF file source from direct access using .htaccess
      *  Or you can never reveal its original file name such as encoding it first!
      */
-      value: "Hakira_Basheni",
+      value: "Final_Problem_Empty_House",
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
